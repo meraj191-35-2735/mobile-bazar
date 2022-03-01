b4af33d3b535e03d30f2f62c99957f648c587c47
meraj191-35-2735 <www.shamsad35-2735@diu.edu.bd>
2022-03-01 21:25:15 +0600

    final commit

--- js/app.js
+++ js/app.js
@@ -15,8 +15,8 @@ const searchPhone = () => {
 const displayPhone = (phones) => {
   const searchResult = document.getElementById("search-result");
   searchResult.textContent = "";
-
-  if (phones.length < 20 && phones.length > 0) {
+  // display which phones array length 20 or less than 20
+  if (phones.length <= 20 && phones.length > 0) {
     const alert = document.getElementById("alert");
     alert.textContent = "";
     for (const phone of phones) {
@@ -32,6 +32,7 @@ const displayPhone = (phones) => {
       </div>`;
       searchResult.appendChild(div);
     }
+    // display which phones array length greater than 20
   } else if (phones.length > 20) {
     const alert = document.getElementById("alert");
     alert.textContent = "";
@@ -48,11 +49,19 @@ const displayPhone = (phones) => {
       </div>`;
       searchResult.appendChild(div);
     }
+    // button for showing all phones which phones array length greater than 20
     const div2 = document.createElement("div");
-    div2.classList.add("d-flex", "align-items-end");
-    div2.innerHTML = `<button onclick="showAll()" class="btn-small btn-light text-primary text-decoration-none fw-bold">Show All</button>`;
+    div2.classList.add(
+      "d-flex",
+      "align-items-end",
+      "justify-content-end",
+      "pb-3"
+    );
+    div2.setAttribute("id", "div2");
+    div2.innerHTML = `<button id="show-all" class="btn-small btn-light text-primary text-decoration-none fw-bold">Show All</button>`;
     searchResult.appendChild(div2);
-  } else {
+  } //condition for invalid input or no user input
+  else {
     const alert = document.getElementById("alert");
     alert.textContent = "";
     const phoneDetails = document.getElementById("phone-details");
@@ -62,6 +71,24 @@ const displayPhone = (phones) => {
     h6.innerText = "No results found❕ Enter a valid phone name 📱";
     alert.appendChild(h6);
   }
+
+  // function for show all phones which array length greater than 20
+  document.getElementById("show-all").addEventListener("click", () => {
+    for (const phone of phones) {
+      const div = document.createElement("div");
+      div.classList.add("col");
+      div.innerHTML = `
+        <div class="card">
+        <img src="${phone.image}" class="card-img-top mx-auto w-50 h-50 pt-3" alt="..." />
+        <div class="card-body mx-auto">
+          <h5 class="card-title">${phone.phone_name}</h5>
+          <button onclick="loadDetail('${phone.slug}')" class="btn btn-primary text-white fw-bold">Show Details</button>
+        </div>
+      </div>`;
+      searchResult.appendChild(div);
+    }
+    document.getElementById("div2").remove();
+  });
 };
 
 // load details results
@@ -79,21 +106,30 @@ const displayPhoneDetail = (phone) => {
   const div = document.createElement("div");
   div.classList.add("card", "mb-3", "w-50", "mx-auto");
   const sensors = phone.mainFeatures.sensors;
+  // condition for show there is all  feature
   if (phone.others !== undefined) {
     div.innerHTML = `
-    <img src="${phone.image}" class="card-img-top w-25 h-25 mx-auto pt-3" alt="..." />
+    <img src="${
+      phone.image
+    }" class="card-img-top w-25 h-25 mx-auto pt-3" alt="..." />
     <div class="card-body">
       <h5 class="card-title fw-bold">${phone.name}</h5>
     <div class="card-text">
     <p>
-       ${phone.releaseDate} <br>
+       ${
+         phone.releaseDate ? phone.releaseDate : "Release Data Unavailable!"
+       } <br>
       <span class='fw-bold'>Brand Name:</span> ${phone.brand}<br>
-      <span class='fw-bold'>Features:</span><br>
+      <span class='fw-bolder text-center text-danger'>Features:</span><br>
       <span class='fw-bold'>Chip set:</span> ${phone.mainFeatures.chipSet}<br>
-      <span class='fw-bold'>Display size:</span> ${phone.mainFeatures.displaySize}<br>
+      <span class='fw-bold'>Display size:</span> ${
+        phone.mainFeatures.displaySize
+      }<br>
       <span class='fw-bold'>Memory:</span> ${phone.mainFeatures.memory}<br>
-      <span class='fw-bold'>Sensors:</span> ${sensors[0]},${sensors[1]},${sensors[2]} <br>
-      <span class='fw-bold'>Others:</span><br>
+      <span class='fw-bold'>Sensors:</span> ${sensors[0]},${sensors[1]},${
+      sensors[2]
+    } <br>
+      <span class='fw-bolder text-center text-danger'>Others:</span><br>
         <span class='fw-bold'>Bluetooth:</span> ${phone.others.Bluetooth}<br>
         <span class='fw-bold'>GPS:</span> ${phone.others.GPS}<br>
         <span class='fw-bold'>NFC:</span> ${phone.others.NFC}<br>
@@ -103,7 +139,8 @@ const displayPhoneDetail = (phone) => {
       </p>  
       </div>
       `;
-  } else {
+  } //condition for show features without sensors
+  else {
     div.innerHTML = `
     <img src="${phone.image}" class="card-img-top w-25 h-25 mx-auto pt-3" alt="..." />
     <div class="card-body">
@@ -112,7 +149,7 @@ const displayPhoneDetail = (phone) => {
     <p>
       ${phone.releaseDate} <br>
       <span class='fw-bold'>Brand Name:</span> ${phone.brand}<br>
-      <span class='fw-bold'>Features:</span><br>
+      <span class='fw-bolder text-center text-danger'>Features:</span><br>
       <span class='fw-bold'>Chip set:</span> ${phone.mainFeatures.chipSet}<br>
       <span class='fw-bold'>Display size:</span> ${phone.mainFeatures.displaySize}<br>
       <span class='fw-bold'>Memory:</span> ${phone.mainFeatures.memory}<br>
